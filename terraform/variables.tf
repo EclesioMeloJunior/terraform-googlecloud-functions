de4e9b7c8f912c2fa3db39ac08b331986ac6dc46
eclesiomelojunior <eclesiomelo.1@gmail.com>
2020-02-06 12:13:10 -0300

    terraform git control version

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,20 @@
 variable "cloudfunctions_bucket" {
-	default = "slack-app-bucket"
+  default = "slack-app-bucket"
 }
 
 variable "secrets_bucket" {
-	default = "secrets_app_bucket"
+  default = "secrets_app_bucket"
 }
 
 variable "pubsub_topic" {
-	default = "NOTIFY_SLACK"
+  default = "NOTIFY_SLACK"
 }
 
 variable "google_project" {
-	default = "slack-app-265014"
+  default = "slack-app-265014"
 }
+
+variable "google_project_credentials" {
+  default = "../.credentials/slack-app-4274e5c8156c.json"
+}
+
